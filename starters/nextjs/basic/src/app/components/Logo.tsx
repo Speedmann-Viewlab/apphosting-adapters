341519282216ee6e5f44c7c06f930d89431e3eb6
Speedmann-Viewlab <christian.schloegel@gmail.com>
2026-05-28 17:19:21 +0200

    Update Logo.tsx

--- starters/nextjs/basic/src/app/components/Logo.tsx
+++ starters/nextjs/basic/src/app/components/Logo.tsx
@@ -1,4 +1,4 @@
-export function Firebase() {
+export function Logo() {
   return (
     <div style="position: absolute;top: -6vh;left: -43vw;height: 33vh;">
         <img src="/logo.png" border="none" height="100%">
