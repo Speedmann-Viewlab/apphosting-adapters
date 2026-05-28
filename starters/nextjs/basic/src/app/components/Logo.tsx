f791e8d393a2021b575fd6e6cc37586301b950a1
Speedmann-Viewlab <christian.schloegel@gmail.com>
2026-05-28 17:28:09 +0200

    Update Logo.tsx

--- starters/nextjs/basic/src/app/components/Logo.tsx
+++ starters/nextjs/basic/src/app/components/Logo.tsx
@@ -2,7 +2,7 @@ export function Logo() {
   return (
     <>
       <div style="position: absolute;top: -6vh;left: -43vw;height: 33vh;">
-          <img src="/logo.png" border="none" height="100%">
+          <img src="/logo.png" border="none" height="100%" />
       </div>
     </>
   );
