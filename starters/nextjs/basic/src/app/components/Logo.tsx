986e633570039df47bf8254c905301d65841c418
Speedmann-Viewlab <christian.schloegel@gmail.com>
2026-05-28 17:00:59 +0200

    Create Logo.tsx

---
+++ starters/nextjs/basic/src/app/components/Logo.tsx
@@ -0,0 +1,7 @@
+export function Firebase() {
+  return (
+    <div style="position: absolute;top: -6vh;left: -43vw;height: 33vh;">
+        <img src="/logo_white.png" border="none" height="100%">
+    </div>
+  );
+}
